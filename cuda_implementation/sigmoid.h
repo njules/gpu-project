8140b1739e92782ade84eb813e086d882452ec9b
DAVIDE BOLOGNESI S295938 <s295938@studenti.polito.it>
2023-01-14 18:52:51 +0100

    c implementation done!

---
+++ cuda_implementation/sigmoid.h
@@ -0,0 +1,10 @@
+#ifndef DEF_SIGMOID
+#define DEF_SIGMOID
+
+#define EULER_NUMBER 2.71828
+#define EULER_NUMBER_F 2.71828182846
+#define EULER_NUMBER_L 2.71828182845904523536
+
+long double sigmoidl(long double n);
+
+#endif
